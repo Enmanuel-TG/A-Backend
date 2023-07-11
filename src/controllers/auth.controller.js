20990697d66a50670238c2b093b5c125ca1a6b99
Enmanuel <62859680+Enmanuel-TG@users.noreply.github.com>
2023-07-11 17:55:16 +0000

    encriptando la contraseñas

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,20 +1,20 @@
 import User from "../models/user.models.js"
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
     const {email, password, username} = req.body
     console.log(email, password, username);
 
     try {
+        const passwordhash = await bcrypt.hash(password, 10)
         const newUser  = new User({
             username,
             email,
-            password
+            password: passwordhash
         })
         console.log(newUser)
         const userSaverd = await newUser.save()
         res.json(userSaverd)
-        res.send("registrando")
-
         
     } catch (error) {
         console.log(error)
